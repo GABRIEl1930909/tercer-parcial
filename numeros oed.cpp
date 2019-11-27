6d661daaed91d5eb12fbf12b8e91e253d2f81848
GABRIEl1930909 <56701176+GABRIEl1930909@users.noreply.github.com>
2019-11-27 09:59:20 -0600

    Add files via upload

---
+++ numeros oed.cpp
@@ -0,0 +1,37 @@
+//Universidad Politecnica de Tulancingo
+//Ing.Electronica y Telecomunicaciones
+//Logica de programacion 
+//Gabriel Gayosso Trejo
+//Arturo Negrete M.
+//Nos servira poara ordenar numeros de mayor a menor "ordenar numeros".
+
+
+#include <stdlib.h>
+#include <stdio.h>
+const int n = 4;
+int main()
+{
+	int matriz[n][n];
+	int ren = 0, col = 0;
+	while (ren < n) {
+		while (col < n) {
+			printf("cptura el elemento [%d,%d] ", ren, col);			
+
+			scanf_s("%D", &matriz[ren][col]);
+			col++;
+
+		}
+
+		ren++; col = 0;
+	}
+	ren = 0;
+	while (ren < n) {
+		col = 0;
+		while (col < n) {
+			printf("%4d", matriz[ren][col]);
+			col++;
+		}
+		printf("\n");
+		ren++;
+	}
+}
